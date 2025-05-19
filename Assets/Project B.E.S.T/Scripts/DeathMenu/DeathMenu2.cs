7b7a9c39facf9c1ef1eadce477bc6b765b2d228f
Edwin Serrano <Ranfuan@yahoo.com>
2025-05-18 22:48:06 -0700

    Added a mini-menu for when character dies.

---
+++ Assets/Project B.E.S.T/Scripts/DeathMenu/DeathMenu2.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathMenu2 : MonoBehaviour
+{
+    public HealthSlider healthSlider;
+    public GameObject DeathPanel;
+    public Text gameOverText;
+    public Button btnYes;
+    public Button btnNo;
+
+    void Start()
+    {
+        // Ensure the panel starts hidden
+        DeathPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (healthSlider.health <= 0)
+        {
+            ShowGameOverScreen();
+        }
+    }
+
+    void ShowGameOverScreen()
+    {
+        DeathPanel.SetActive(true);
+        gameOverText.text = "Would you like to retry?";
+    }
+}
---
+++ Assets/Project B.E.S.T/Scripts/DeathMenu/LoadMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadMenu : MonoBehaviour
+{
+	
+	public void MenuLoad()
+	{
+		SceneManager.LoadSceneAsync(0);
+	}
+	
+}
